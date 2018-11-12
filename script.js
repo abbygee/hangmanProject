695295021995478791058c21ec815a41b12011c9
abbygee <abigaildelgado@students.berkeley.net>
2018-11-12 12:30:01 -0800

    fixed functions

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 var word = "";
-var words = ["pink", "red", "maroon", "turquoise"];
+var easy = ["pink", "red", "green", "blue", "white", "black", "yellow", "orange"];
+var med = ['maroon', 'turquoise', 'beige', 'magenta', 'lavender', 'mauve', 'fuchsia', 'sapphire', 'slate'];
+var hard = ['eminence', 'cordovan', 'cinnabar', 'mikado', 'cerulean', 'amaranth'];
 var guesses = 6;
 var guessedLetters = [];
 
 function startGame(){
+    //restarts the game
+    word = "";
+    guesses = 6;
+    guessedLetters = [];
 
-    var empty = "";
-    var rand = words[Math.floor(Math.random() * words.length)];
-    for(var i = 0; i < rand.length; i++){
-        empty += "_ ";
-    }
-    document.getElementById('guessed').innerHTML = empty;
-    document.getElementById('numGuesses').innerHTML = guesses.toString();
-}
+    //determines words from the difficulty the user chose
+    var cat = document.getElementById('cat').value;
 
-function guessLetter(letter){
-    if(word.indexOf(letter) === -1){
-        guesses--;
-    }
+    if(cat === 'default'){
+        alert('Please choose a difficulty before continuing');
+    }else{
+        if(cat === 'Easy'){
+            word = easy[Math.floor(Math.random() * easy.length)];
+        }
+        if(cat === 'Medium'){
+            word = med[Math.floor(Math.random() * med.length)];
+        }
+        if(cat === 'Hard'){
+            word = hard[Math.floor(Math.random() * hard.length)];
+        }
+        //states the blank lines of the user's word
+        var empty = "";
+        for(var i = 0; i < word.length; i++){
+            empty += "_ ";
+        }
 
-    if(guesses > 0){
-        guessedLetters += letter;
-        var answer = printWord();
-        document.getElementById('guessed').innerHTML = answer;
+        //prints the word and guesses to user
+        document.getElementById('guessed').innerHTML = empty;
+        document.getElementById('numGuesses').innerHTML = guesses.toString();
     }
 }
 
@@ -37,4 +49,17 @@ function printWord(){
         }
     }
     return answer;
+}
+
+function guessLetter(letter){
+    if(word.indexOf(letter) === -1){
+        guesses--;
+    }
+
+    document.getElementById('numGuesses').innerHTML = guesses.toString();
+
+    if(guesses > 0){
+        guessedLetters.push(letter);
+        document.getElementById('guessed').innerHTML = printWord();
+    }
 }
